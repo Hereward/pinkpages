4db320e7be65f5a84d9d1e500360c6dd99f36f62
sydneypink <sydneypink@b8080cc7-2fde-4e2d-9b89-36de1267afd3>
2012-02-07 04:16:55 +0000

    git-svn-id: http://svn.sydneypinkpages.com.au/pinkpages/trunk@224 b8080cc7-2fde-4e2d-9b89-36de1267afd3

--- app/Control/ListingControl.class.php
+++ app/Control/ListingControl.class.php
@@ -227,7 +227,7 @@ class ListingControl extends MainControl {
      */
 		public function search()
 		{
-            dev_log::cur_url("Listing::search");
+  
 			$this->page->pageTitle 			= "Pink Pages &shy; Search results for [".$_GET['Search1']."] ";
 			$do         				= $_GET['do'];
 			$action						= $_GET['action'];
@@ -939,7 +939,8 @@ class ListingControl extends MainControl {
 		     $this->page->assign("regionURLs", $regionURLs);			 		     				 
 		}
 		else {
-			$location = 'All Sydney';
+			$location = $_GET['state'];
+			// $location = 'All Sydney';
 		}		
 		
 		//Assign suburb/region search area
