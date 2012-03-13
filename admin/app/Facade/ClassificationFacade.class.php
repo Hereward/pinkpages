2b911976f41eb976d1956599802ebdb958797337
sydneypink <sydneypink@b8080cc7-2fde-4e2d-9b89-36de1267afd3>
2012-03-13 04:34:34 +0000

    git-svn-id: http://svn.sydneypinkpages.com.au/pinkpages/trunk@456 b8080cc7-2fde-4e2d-9b89-36de1267afd3

--- admin/app/Facade/ClassificationFacade.class.php
+++ admin/app/Facade/ClassificationFacade.class.php
@@ -910,7 +910,7 @@ class ClassificationFacade extends MainFacade {
         	var_dump($result_set);
         	$end_time = time();
         	$tot_time = $end_time-$start_time;
-        	die("Operation took $tot_time seconds");
+        	die("Operation took $tot_time seconds | GOOGLE FILTER:[$google_filter]");
         	$current_ts = mktime(0, 0, 0, date("m",$current_ts), date("d",$current_ts)+1, date("Y",$current_ts));
         }
         var_dump($result_set);
