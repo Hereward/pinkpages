b53dce82af5b563f4deef589b6c9e2699156fa0a
sydneypink <sydneypink@b8080cc7-2fde-4e2d-9b89-36de1267afd3>
2012-03-21 00:20:39 +0000

    git-svn-id: http://svn.sydneypinkpages.com.au/pinkpages/trunk@520 b8080cc7-2fde-4e2d-9b89-36de1267afd3

--- admin/app/Control/AdminListingControl.class.php
+++ admin/app/Control/AdminListingControl.class.php
@@ -487,6 +487,7 @@ class AdminListingControl extends MainControl {
 		$this->page->assign("addbusinessform",$this->request->createURL("SalesAccountManager", "addListing"));
 	    $this->page->assign("search",$this->request->createURL("SalesAccountManager","searchBusiness"));
 	    $this->page->assign("class_relationships",$this->request->createURL("AdminListing","class_relationships"));
+	    $this->page->assign("export_class_relationships_action",$this->request->createURL("AdminListing","export_class_relationships"));
 		$this->page->getPage("class_relationships.tpl");
 	}
 	
