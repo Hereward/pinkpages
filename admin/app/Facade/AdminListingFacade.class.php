1c55cdcb73b6533e837e5d7d3ee596028882d18f
sydneypink <sydneypink@b8080cc7-2fde-4e2d-9b89-36de1267afd3>
2012-03-25 23:34:06 +0000

    git-svn-id: http://svn.sydneypinkpages.com.au/pinkpages/trunk@544 b8080cc7-2fde-4e2d-9b89-36de1267afd3

--- admin/app/Facade/AdminListingFacade.class.php
+++ admin/app/Facade/AdminListingFacade.class.php
@@ -967,9 +967,10 @@ class AdminListingFacade extends MainFacade {
 							$num = count($data);
 							dev_log::write("class_relationships_upload -  ".var_export($data, true));
 							//echo "<p> $num fields in line $row: <br /></p>\n";
+							dev_log::write("class_relationships_upload DATA[0] -  ".$data[0]);
 							$row++;
 							if ($row > 1) { break;}
-							dev_log::write("class_relationships_upload DATA[0] -  ".$data[0]);
+							
 							$arr = explode($data[0], $data);
 							
 							$class_id = $arr[0];
