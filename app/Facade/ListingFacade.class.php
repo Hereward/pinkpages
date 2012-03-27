efdf97b5936bb8d3a700769ab52a3b53d553308d
sydneypink <sydneypink@b8080cc7-2fde-4e2d-9b89-36de1267afd3>
2012-03-27 01:02:28 +0000

    git-svn-id: http://svn.sydneypinkpages.com.au/pinkpages/trunk@591 b8080cc7-2fde-4e2d-9b89-36de1267afd3

--- app/Facade/ListingFacade.class.php
+++ app/Facade/ListingFacade.class.php
@@ -3450,10 +3450,11 @@ class ListingFacade extends MainFacade {
 	public function relatedClassLinks($class_id) {
 		$query = "SELECT * from class_relationships WHERE class_id='$class_id'";
 		dev_log::write("relatedClassLinks query = ".$query);
-		$res	= $this->myDB->query($query);
+		$res = $this->myDB->query($query);
 		$list = '';
 	    if($res) {
 	    	$list = $res[0]['related'];
+	    	dev_log::write("relatedClassLinks list = ".$list);
 			$output = explode(',', $list);
 		}
 		
