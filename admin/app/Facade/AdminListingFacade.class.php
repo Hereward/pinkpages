5e6a4b1fcccfc3230b14530b6085d4f15131b20f
sydneypink <sydneypink@b8080cc7-2fde-4e2d-9b89-36de1267afd3>
2012-03-28 04:44:03 +0000

    git-svn-id: http://svn.sydneypinkpages.com.au/pinkpages/trunk@620 b8080cc7-2fde-4e2d-9b89-36de1267afd3

--- admin/app/Facade/AdminListingFacade.class.php
+++ admin/app/Facade/AdminListingFacade.class.php
@@ -1035,7 +1035,7 @@ class AdminListingFacade extends MainFacade {
 	public function insertCSV($post)
 	{
 
-
+        dev_log::timer('set');
 		$success = 0;
 		$failure = 0;
 		//Remove Existing Entries
@@ -1272,6 +1272,7 @@ class AdminListingFacade extends MainFacade {
 		//Delete all Entries from the FREE_BUSINESSES TABLE. DATABASE WILL ENFORCE REFERENTIAL INTEGRITY with FREEBUSINESS_CLASSIFICATION table
 		$sql02 = "delete from local_businesses where business_initials = 'Free' AND business_id IN ($in_string) AND business_state='$state'";
 		dev_log::write($sql02);
+		dev_log::timer('get');
 		die('deleteFreeListingsClassification');
 		//$result  = $this->MyDB->query($sql_02);
 	}
