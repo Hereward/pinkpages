28cb8b836ab1e64303049573f9fa4186fb945a56
sydneypink <sydneypink@b8080cc7-2fde-4e2d-9b89-36de1267afd3>
2012-07-10 01:02:04 +0000

    git-svn-id: http://svn.sydneypinkpages.com.au/pinkpages/trunk@1019 b8080cc7-2fde-4e2d-9b89-36de1267afd3

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 include("System/Config/config.php");
 include("app_config.php");
 include("app_boot.php");
-dev_log::cur_url();
+//dev_log::cur_url();
 
 if ( isValidSession() ) {
 	$do = DEFAULT_CONTROL_VS;
