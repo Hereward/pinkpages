d37dcc9f7e03563fa970c77363b4910ea72f7ee3
sydneypink <sydneypink@b8080cc7-2fde-4e2d-9b89-36de1267afd3>
2012-09-04 05:39:10 +0000

    git-svn-id: http://svn.sydneypinkpages.com.au/pinkpages/trunk@1240 b8080cc7-2fde-4e2d-9b89-36de1267afd3

--- admin/app/Facade/SalesAccountManagerFacade.class.php
+++ admin/app/Facade/SalesAccountManagerFacade.class.php
@@ -1873,6 +1873,7 @@ class SalesAccountManagerFacade extends MainFacade {
 						business_faxstd='{$post['faxstd']}',
 						business_fax='{$post['fax']}',
 						business_email='{$post['email']}',
+						reporting_email='{$post['reporting_email']}',
 						business_url='{$post['url']}',
 						shiretown_id=null,
 						business_mobile='{$post['mobile']}',
